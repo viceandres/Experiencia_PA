121ca139f2ff4e9147267a2ff5c441f26e001326
renatocabello <163414534+renatocabello@users.noreply.github.com>
2024-03-14 18:28:47 +0000

    lISTO EL JUEGO

--- juegos/juego_del_dado.py
+++ juegos/juego_del_dado.py
@@ -1,8 +1,18 @@
+import random
 def juego_del_dado():
-    """
-    Esta función tiene que pedirle al usuario que aprete enter para que lance un dado.
-    Esto genera un número al azar que se le suma a la puntuación del usuario.
-    Después el computador también tiene que lanzar un dado.
-    El primero en sumar 30 puntos gana.
-    """
-    pass
+    p_hombre=0
+    p_pc=0
+    while p_hombre<30 or p_pc<30:
+        input("Presiona Enter para lanzar el dado")
+        numero=random.randint(1, 6)
+        p_hombre+=numero
+        print(" tu numero es "+str(numero)+" y tu puntuacion "+str(p_hombre))
+        if p_hombre>=30:
+            print("gana hombre")
+            break
+        numero=random.randint(1, 6)
+        p_pc+=numero
+        print(" el numero del pc es "+str(numero)+" y su puntuacion "+str(p_pc))
+        if p_pc>=30:
+            print("gana pc")
+            break
