d5555eb600c613270eac648f651a370920b748b4
mconcha2003 <163340748+mconcha2003@users.noreply.github.com>
2024-03-14 18:43:58 +0000

    listoco

--- juegos/memoria.py
+++ juegos/memoria.py
@@ -1,8 +1,11 @@
+import random
 def memoria():
-    """
-    Esta función representa el juego de memoria.
-    Debes generar una secuencia de números al azar y mostrarla al usuario.
-    Luego, debes pedir al usuario que repita la secuencia.
-    Se debe mostrar un mensaje si el usuario acierta o no.
-    """
-    pass
+    frase=""
+    for i in range(5):
+        num=random.randint(0,9)
+        frase+=str(num)
+    je=input("adivina la secuencia de numeros/n")
+    if je==frase:
+        return(print("GANASTEEEEE"))
+    else:
+        return(print("es dificil adivinar una secuencia de 5 numeros"))
